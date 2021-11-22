05aef27d62e6607f4159cd72e0a7313bdac7ef54
Hoa7102002 <thihoa.maicogroup@gmail.com>
2021-11-22 21:26:32 +0700

    helloword

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello world"<<endl;
+    int i=0;
+    cout<<"nhap vao tuoi cua ban:";
+    cin>> i;
+    cout<<i;
+    system("pause");
+    return 0;
+}
